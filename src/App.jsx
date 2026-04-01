4dad8cc622ee7adb17702b5764ba650b4269bc82
ltaverni <>
2026-04-01 16:08:28 +0200

    (feat) - handle forms and button to update values

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
+import { useState } from 'react'
 import './App.css'
 
-const ResultLine = () => {
+const calculateTip = (bill, tip) => {
+  return (bill * (0.01 * tip));
+}
+
+const ResultLine = ({name, value}) => {
   return (
     <div className='result-line'>
       <div>
-        <p className='result-type'>Type of data</p>
+        <p className='result-type'>{name}</p>
         <p className='pro-person'>/ person</p>
       </div>
-      <div className='amount'>$4.27</div>
+      <div className='amount'>${value}</div>
     </div>
   )
 }
 
-const Results = () => {
+const Results = ({tip, peopleNumber, handleReset}) => {
+  const tipProPerson = (peopleNumber === 0) ? 0 : tip / peopleNumber;
+
   return (
     <div className='results'>
-      <ResultLine />
-      <ResultLine />
-      <button>RESET</button>
+      <ResultLine name='Tip Amount' value={tipProPerson} />
+      <ResultLine name='Total' value={tip} />
+      <button onClick={handleReset}>RESET</button>
     </div>
   )
 }
 
-const Tip = () => {
+const Tip = ({tip, setParameter, setTip}) => {
   return (
     <div className='tip'>
       <p>Select Tip %</p>
@@ -32,49 +39,105 @@ const Tip = () => {
         <div className='tip-elem'>15%</div>
         <div className='tip-elem'>25%</div>
         <div className='tip-elem'>50%</div>
-        <input className='tip-elem tip-elem-custom' type='text' placeholder='Custom' />
+        <input className='tip-elem tip-elem-custom' type='text' placeholder='Custom' value={tip} onChange={(e) => setParameter(e, setTip)} />
       </div>
     </div>
   )
 }
 
-const ChoiceLine = () => {
+const ChoiceLine = ({name, value, setParameter, setFunction}) => {
   return (
     <div className='choice'>
-      <p>Type of choice</p>
+      <p>{name}</p>
       <div className='choiceLine'>
         <img></img>
-        <input className='choice-input' type='text' value='142.55 / 5' />
+        <input className='choice-input' type='text' value={value} onChange={(e) => setParameter(e, setFunction)} />
       </div>
     </div>
   )
 }
 
-const Parameters = () => {
+const Parameters = ({bill, tip, peopleNumber, setParameter, setBill, setTip, setPeopleNumber}) => {
   return (
     <div className='parameters'>
-      <ChoiceLine />
-      <Tip />
-      <ChoiceLine />
+      <ChoiceLine
+        name='Bill'
+        value={bill}
+        setParameter={setParameter}
+        setFunction={setBill} />
+      <Tip
+        tip={tip}
+        setParameter={setParameter}
+        setTip={setTip} />
+      <ChoiceLine
+        name='Number of People'
+        value={peopleNumber}
+        setParameter={setParameter}
+        setFunction={setPeopleNumber} />
     </div>
   )
 }
 
-const Card = () => {
+const Card = ({bill, tip, peopleNumber, setParameter, setBill, setTip, setPeopleNumber, handleReset}) => {
+  const tipValue = calculateTip(bill, tip);
+
   return (
     <div className='card'>
-      <Parameters />
-      <Results />
+      <Parameters
+        bill={bill}
+        tip={tip}
+        peopleNumber={peopleNumber}
+        setParameter={setParameter}
+        setBill={setBill}
+        setTip={setTip}
+        setPeopleNumber={setPeopleNumber} />
+      <Results tip={tipValue} peopleNumber={peopleNumber} handleReset={handleReset} />
     </div>
   )
 }
 
 const App = () => {
+  const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [peopleNumber, setPeopleNumber] = useState(0);
+
+  const setParameter = (e, setFunction) => {
+    e.preventDefault();
+    
+    // Checks to ensure that values enterred are only numbers
+    const input = e.target.value; 
+    if (input.includes(" ") || input.startsWith("0"))
+      return
+    
+    const updateValue = Number(e.target.value);
+    if (isNaN(updateValue)) {
+      console.log("NaN !!")
+      return;
+    }
+
+    setFunction(e.target.value);
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault();
+
+    setBill(0);
+    setTip(0);
+    setPeopleNumber(0);
+  }
 
   return (
     <>
       <img></img>
-      <Card />
+      <Card 
+        bill={bill}
+        tip={tip}
+        peopleNumber={peopleNumber}
+        setParameter={setParameter}
+        setBill={setBill}
+        setTip={setTip}
+        setPeopleNumber={setPeopleNumber}
+        handleReset={handleReset} />
     </>
   )
 }
